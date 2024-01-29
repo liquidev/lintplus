bdc4d6c8755c52bdcb7eb2f09d25882e8ea53a02
PerilousBooklet <raffaele.orabona@protonmail.com>
2024-01-29 22:16:47 +0100

    Added latex linting support.

---
+++ linters/lacheck.lua
@@ -0,0 +1,18 @@
+lintplus.add("lacheck") {
+  filename = "%.tex$",
+  procedure = {
+    command = lintplus.command {
+      "lacheck", "", lintplus.filename
+    },
+    interpreter = lintplus.interpreter {
+      -- The format for these three in Nim is almost exactly the same:
+      hint = "(.-)%((%d+), (%d+)%) Hint: (.+)",
+      warning = "(.-)%((%d+), (%d+)%) Warning: (.+)",
+      error = "(.-)%((%d+), (%d+)%) Error: (.+)",
+      -- We want to strip annotations like [XDeclaredButNotUsed] from the end:
+      strip = "%s%[%w+%]$",
+      -- Note that info was omitted. This is because all of the severity levels
+      -- are optional, so eg. you don't have to provide an info pattern.
+    },
+  },
+}
