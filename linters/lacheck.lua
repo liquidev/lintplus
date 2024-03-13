7b5f03c3c57811d6a5faa23680e6edd64b59f808
PerilousBooklet <raffaele.orabona@protonmail.com>
2024-03-13 17:10:47 +0100

    WIP: modified warning pattern.

--- linters/lacheck.lua
+++ linters/lacheck.lua
@@ -7,14 +7,11 @@ lintplus.add("lacheck") {
       "lacheck", lintplus.filename
     },
     interpreter = lintplus.interpreter {
-      -- The format for these three in Nim is almost exactly the same:
-      hint = "(.-)%((%d+), (%d+)%) Hint: (.+)",
-      warning = "(.-)%((%d+), (%d+)%) Warning: (.+)",
-      error = "(.-)%((%d+), (%d+)%) Error: (.+)",
-      -- We want to strip annotations like [XDeclaredButNotUsed] from the end:
-      strip = "%s%[%w+%]$",
-      -- Note that info was omitted. This is because all of the severity levels
-      -- are optional, so eg. you don't have to provide an info pattern.
-    },
-  },
+      warning = string.format('"%s"', lintplus.filename) .. "," .. "\\sline:\\s\\d+\\:\\s[a-z]+"
+    }
+  }
 }
+
+lintplus.setup.lint_on_doc_load()  -- enable automatic linting upon opening a file
+lintplus.setup.lint_on_doc_save()  -- enable automatic linting upon saving a file
+lintplus.load({"lacheck"})
