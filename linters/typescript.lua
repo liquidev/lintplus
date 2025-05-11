15476d1dd60110b5801656bcb26a299945d9e756
Andrei Vinca <andrei.vinca@outlook.com>
2025-05-11 23:04:12 +0300

    Added Typescript linter (#36)
    * Added Typescript linter
    
    * Update typescript.lua

---
+++ linters/typescript.lua
@@ -0,0 +1,30 @@
+-- Typescript lint plugin for lint+
+
+--- CONFIG ---
+
+--- __Site__: https://github.com/eslint/eslint
+--- __Installation__: `npm install -g eslint typescript --save-dev`
+--- __Local Config__: [optional] npx eslint --init
+
+--- IMPLEMENTATION ---
+local lintplus = require "plugins.lintplus"
+
+lintplus.add("typescript") {
+  filename = "%.ts$",
+  procedure = {
+    
+    command = lintplus.command(
+      { 
+        "eslint",
+        "--rule", "{}",
+        "--format", "visualstudio",
+        lintplus.filename
+      }
+    ),
+    
+    interpreter = lintplus.interpreter ({
+      warning = "^(.+)%((%d+),(%d+)%)%: warning%s?[^:]*: (.+)$",
+      error = "^(.+)%((%d+),(%d+)%)%: error%s?[^:]*: (.+)$"
+    }),
+  }
+}
